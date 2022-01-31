efd3da95ab12dee14b71fc98fa2381e3ee12b662
EmotionChild <JDRCaple@outlook.com>
2022-01-31 23:08:20 +1300

    first commit

---
+++ blog/2019-05-28-first-blog-post.md
@@ -0,0 +1,12 @@
+---
+slug: first-blog-post
+title: First Blog Post
+authors:
+  name: EmotionChild
+  title: Lead Developer and Project Lead
+  url: https://www.emotionchild.com
+  image_url: https://github.com/EmotionChild.png
+tags: [hola, docusaurus]
+---
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum dignissim ultricies. Fusce rhoncus ipsum tempor eros aliquam consequat. Lorem ipsum dolor sit amet
---
+++ docusaurus.config.js
@@ -0,0 +1,111 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Ellie Blog',
+  tagline: 'Here you can find blog posts about Ellie and other things we are up to',
+  url: 'https://blog.emotionchild.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'EmotionChild', // Usually your GitHub org/user name.
+  projectName: 'Ellie-Blog', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/EmotionChild/Ellie-Blog/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/EmotionChild/Ellie-Blog/tree/main/packages/create-docusaurus/templates/shared/',
+          blogTitle: 'Ellie Blog',
+          blogDescription: 'Here you can find blog posts about Ellie and other things we are up to',
+          postsPerPage: 'ALL',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Ellie Blog',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/EmotionChild/Ellie-Blog',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/SVQVzJq',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/Computergeex5',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/EmotionChild/Ellie-Blog',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} EmotionChild. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
